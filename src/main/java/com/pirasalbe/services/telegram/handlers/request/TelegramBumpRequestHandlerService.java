3374f789064b13eba47f4c9fab4b71a963ac2f27
pirasalbe <alberto98.piras@gmail.com>
2022-08-23 14:05:08 +0200

    Add check bump keyword

--- src/main/java/com/pirasalbe/services/telegram/handlers/request/TelegramBumpRequestHandlerService.java
+++ src/main/java/com/pirasalbe/services/telegram/handlers/request/TelegramBumpRequestHandlerService.java
@@ -32,7 +32,7 @@ public class TelegramBumpRequestHandlerService extends AbstractTelegramRequestHa
 
 	protected static final List<String> BUMPS = Arrays.asList("bump", "update", "can i get", "can i have", "need",
 			"please help", "send", "thank", "repost", "news", "anyone", "hope", "this one", "not received", "status",
-			"any luck", "available", "do you", "please anybody");
+			"any luck", "available", "do you", "please anybody", "check");
 
 	@Autowired
 	private SchedulerService schedulerService;
